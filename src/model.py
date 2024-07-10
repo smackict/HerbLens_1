b67eda10b5aa66735fc8156f302f9e2a6ee5888e
smackict <138157453+smackict@users.noreply.github.com>
2024-07-10 15:58:39 +0000

    changed dir orders

--- src/model.py
+++ src/model.py
@@ -5,23 +5,14 @@ import tensorflow as tf
 
 classes = ['bellflower','common_daisy','rose','sunflower']
 
-json_file = open('src/model/model.json', 'r')
+json_file = open('/opt/render/project/src/model/model.json', 'r')
 loaded_model_json = json_file.read()
 json_file.close()
 print('JSON model loaded from disk!')
-model = model_from_json(loaded_model_json)
-model.load_weights("src/model/model.h5")
+loaded_model = model_from_json(loaded_model_json)
+loaded_model.load_weights("/opt/render/project/src/model/cnn_weights.best.weights.h5")
 print("Loaded weights from disk!")
 
-
-# load json and create model
-json_file = open('model.json', 'r')
-loaded_model_json = json_file.read()
-json_file.close()
-loaded_model = model_from_json(loaded_model_json)
-# load weights into new model
-loaded_model.load_weights("cnn_weights.best.weights.h5")
-print("Loaded model from disk")
 
 def predict_image(img_path):
   img = tf.keras.utils.load_img(img_path, target_size=(224, 224))
