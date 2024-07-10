19a76de383c4890ece98e7515c67f9ee4697cc4e
smackict <138157453+smackict@users.noreply.github.com>
2024-07-10 19:09:56 +0000

    Blah blah blah

--- src/model.py
+++ src/model.py
@@ -5,12 +5,12 @@ import tensorflow as tf
 
 classes = ['bellflower','common_daisy','rose','sunflower']
 
-json_file = open('/src/model/model.json', 'r')
+json_file = open('/opt/render/project/src/model/model.json', 'r')
 loaded_model_json = json_file.read()
 json_file.close()
 print('JSON model loaded from disk!')
 loaded_model = model_from_json(loaded_model_json)
-loaded_model.load_weights("/src/model/cnn_weights.best.weights.h5")
+loaded_model.load_weights("/opt/render/project/src/model/cnn_weights.best.weights.h5")
 print("Loaded weights from disk!")
 
 
